1074d9d9bb3180d9fab058d593ebb19abe0ac5ed
Divine Abbah <abbahattamah@gmail.com>
2024-02-10 10:10:11 +0100

    Update on Urls.py file

--- api_crud/urls.py
+++ api_crud/urls.py
@@ -1,10 +1,16 @@
+from django.urls import path
 
-from django.contrib import admin
-from django.urls import include, path
+from . import views
 
-# urls
+
 urlpatterns = [
-    path('api/v1/movies/', include('movies.urls')),
-    path('api/v1/auth/', include('authentication.urls')),
-    path('admin/', admin.site.urls),
+
+  path('', views.index, name='index'),
+
+  path('api/v1/movies/', include('movies.urls')),
+
+  path('api/v1/auth/', include('authentication.urls')),
+
+  path('admin/', admin.site.urls),
+
 ]
