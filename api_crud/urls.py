4e387d37b058adcf07402870c0c4dac897b159d0
Divine Abbah <abbahattamah@gmail.com>
2024-02-10 10:19:44 +0100

    debug fix

--- api_crud/urls.py
+++ api_crud/urls.py
@@ -1,16 +1,11 @@
-from django.urls import path
-
-from . import views
-
+from django.urls import include, path
 
 urlpatterns = [
-
   path('', views.index, name='index'),
 
   path('api/v1/movies/', include('movies.urls')),
 
   path('api/v1/auth/', include('authentication.urls')),
 
-  path('admin/', admin.site.urls),
-
+  path('admin/', admin.site.urls), 
 ]
